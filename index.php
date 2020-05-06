eb5a1dbcd6293ddfe344cd2f54cb5ce115ffd40b
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-06 17:55:06 -0400

    added more

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ session_start();
 
 //Require the autoload file
 require_once("vendor/autoload.php");
+require_once("model/data-layer.php");
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -49,13 +50,15 @@ $f3->route('GET /lunch', function() {
 $f3->route('GET|POST /order', function($f3) {
     //echo '<h1>breakfast page.</h1>';
 
+    $meals = getMeals();
+
     //If the form has been submitted
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //var_dump($_POST);
         //["food"]=>"tacos" ["meal"]=>"lunch"
 
         //Validate the data
-        $meals = array("breakfast", "lunch", "dinner");
+
         if (empty($_POST['food']) || !in_array($_POST['meal'], $meals)) {
             echo "<p>Please enter a food and select a meal</p>";
         }
@@ -65,15 +68,38 @@ $f3->route('GET|POST /order', function($f3) {
             $_SESSION['food'] = $_POST['food'];
             $_SESSION['meal'] = $_POST['meal'];
 
-            //Redirect to summary page
-            $f3->reroute('summary');
+            //Redirect to order2 page
+            $f3->reroute('order2');
             session_destroy();
         }
     }
 
-    //var_dump($_POST);
+    $f3->set('meals', $meals);
+
     $views = new Template();
     echo $views->render('views/order.html');
+});
+
+//order 2 route
+$f3->route('GET /order2', function($f3) {
+
+    $conds = getCondiments();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['conds'] = $_POST['conds'];
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+
+    }
+
+    $f3->set('conds', $conds);
+
+    $views = new Template();
+    echo $views->render('views/orderForm2.html');
 });
 
 //Breakfast route
